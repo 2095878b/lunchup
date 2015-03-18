4ff9784717427d138ff55920feac014796418b30
2095878b <Justas.B94@gmail.com>
2015-03-18 12:38:46 +0000

    Fixed bug in urls, added registration templates.

--- main/views.py
+++ main/views.py
@@ -1,32 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from main.models import *
-# Create your views here.
-
 
 def splash(request):
     return render(request, 'splash.html')
+
 
 def index(request):
     context_dict = {}
     return render(request, 'main/index.html', context_dict)
 
+
 def about(request):
     return render(request, 'main/about.html')
+
 
 def profile(request):
     u = User.objects.get(username=request.user.username)
     context_dict = {}
     try:
-	up = UserProfile.objects.get(user=u)
+        up = UserProfile.objects.get(user=u)
     except:
-	   up = None
-	
+        up = None
+
     context_dict['user'] = u
     context_dict['userprofile'] = up
     return render(request, 'main/profile.html', context_dict)
 
-def register_profile(request):
+
+'''def register_profile(request):
     if request.method == 'POST':
         profile_form = UserProfileForm(data=request.POST)
         if profile_form.is_valid():
@@ -42,15 +44,18 @@ def register_profile(request):
     else:
         profile_form = UserProfileForm()
     return render(request, 'registration/profile_registration.html', {'profile_form': profile_form})
-
-	# Timetabling
+'''
+# Timetabling
 def availability(request):
     context_dict = {}
     return render(request, 'main/availability.html', context_dict)
 
+
 def messages(request):
     context_dict = {}
     return render(request, 'main/message.html', context_dict)
+
+
 # Having the ability to browse lunch history could be useful
 def history(request):
     context_dict = {}
