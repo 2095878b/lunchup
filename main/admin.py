9b67aead127df08be4e8146e2613c612c6e3f380
2080123V <2080123V@student.gla.ac.uk>
2015-03-18 16:51:03 +0000

    models added to the admin page of site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from main.models import Interest, University, UserProfile
+from main.models import Lunch, Feedback, TimeInterval, Message
+                        
+						
 # Register your models here.
+
+admin.site.register(Interest)
+admin.site.register(University)
+admin.site.register(UserProfile)
+admin.site.register(Lunch)
+admin.site.register(Feedback)
+admin.site.register(TimeInterval)
+admin.site.register(Message)
