a95f26cdc4685fa1040ba2a242a1969313e1127c
Omar Tufail <omar.tufail@gmail.com>
2015-03-23 15:15:13 +0000

    No changes that need noted, just been looking through these files

--- lunchup/urls.py
+++ lunchup/urls.py
@@ -2,20 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from lunchup import settings
 from registration.backends.simple.views import RegistrationView
-from tastypie.api import Api
-
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
         return '/'
-
 
 urlpatterns = patterns('',
     url(r'^', include('main.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^ajaximage/', include('ajaximage.urls')),
+    # url(r'^ajaximage/', include('ajaximage.urls')),
 )
 
 if settings.DEBUG:
