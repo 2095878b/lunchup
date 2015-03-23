e0b387072d630bffe0c1228a4bb0f594a9546fdc
Justas <Justas.B94@gmail.com>
2015-03-23 17:50:10 +0000

    Bug in urls.py

--- lunchup/urls.py
+++ lunchup/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from lunchup import settings
 from registration.backends.simple.views import RegistrationView
-<<<<<<< HEAD
-=======
-
->>>>>>> ede1509d0cd9e3491d4689d50287e649549bffea
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
@@ -16,10 +12,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
-<<<<<<< HEAD
     # url(r'^ajaximage/', include('ajaximage.urls')),
-=======
->>>>>>> ede1509d0cd9e3491d4689d50287e649549bffea
 )
 
 if settings.DEBUG:
