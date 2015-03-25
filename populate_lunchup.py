b7b5ad20c77c90b4fb2ead9c5de44dac9e239a42
2080123V <2080123V@student.gla.ac.uk>
2015-03-25 15:40:03 +0000

    more users and stuff

--- populate_lunchup.py
+++ populate_lunchup.py
@@ -16,9 +16,9 @@ def add_user(username, email, password):
         u = User.objects.create_user(username, email, password)
     return u
 
-def add_user_profile(user, fullname, email, about, interests):
+def add_user_profile(user, fullname, email, about, interests, picture):
     p = UserProfile.objects.get_or_create(user=user, fullName=fullname,
-                                          publicEmail=email, about=about, interests=interests)[0]
+                                          publicEmail=email, about=about, interests=interests, picture=picture)[0]
     return p
 
 
@@ -41,7 +41,7 @@ def add_time(time, day):
 
 def add_notification(user1, user2, accepted1, accepted2):
     n = Notification.objects.get_or_create(userOne=user1, userTwo=user2,
-                                           acceptedOne=accepted1, acceptedTwo=accepted2, picture=None)[0]
+                                           acceptedOne=accepted1, acceptedTwo=accepted2)[0]
     return n
 
 
@@ -50,19 +50,6 @@ def add_notification(user1, user2, accepted1, accepted2):
 #    return i
 
 
-# O(n^2), there's room for improvement
-def make_matches():
-    allprofiles = UserProfile.objects.all()
-    apL = len(allprofiles)
-    for user1 in range(0, apL/2 + 1):
-        for user2 in range(apL - 1, apL/2 - 1, -1):
-            if user1 != user2:
-                if allprofiles[user1].university == allprofiles[user2].university:
-                    for t in allprofiles[user1].availability.all():
-                        if t in allprofiles[user2].availability.all():
-                            n = add_notification(allprofiles[user1], allprofiles[user2], False, False)
-                            n.available.add(t)
-                            n.save()
 def populate():
     # Populate time intervals
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -72,49 +59,102 @@ def populate():
             add_time(time=time, day=day)
     # Get some times
     sat13 = add_time(time=13, day='Saturday')
-    wed15 = add_time(time=13, day='Wednesday')
-    thu16 = add_time(time=13, day='Thursday')
-    fri14 = add_time(time=13, day='Friday')
+    wed15 = add_time(time=15, day='Wednesday')
+    thu16 = add_time(time=16, day='Thursday')
+    fri14 = add_time(time=14, day='Friday')
+    tue13 = add_time(time=13, day='Tuesday')
+    thu15 = add_time(time=15, day='Thursday')
+
 
     # Add unis
     glasgow = add_university(university='Glasgow University')
+
+    # Add notifications
+    omju = add_notification(om, ju, True,True)
+    omju.available.add(sat13)
+    amom = add_notification(om, am,True,True)
+    amom.available.add(fri14)
+    juam = add_notification(ju, am, True, True)
+    juam.available.add(wed15)
 
     # Users
     om = add_user('omar', '2098877q@student.gla.ac.uk', 'root')
     om.save()
     ju = add_user('eust', '2095666e@student.gla.ac.uk', 'root')
     ju.save()
-    am = add_user('amy', '2195152a@student.gla.ac.uk', 'root')
+    am = add_user('amyx', '2195152a@student.gla.ac.uk', 'root')
     am.save()
+    ra = add_user('vj', '2080123v@student.gla.ac.uk', 'root')
+    ra.save()
+    bl = add_user_profile('bliatch', '2078910A@student.gla.ac.uk', 'root')
+    bl.save()
+    zo = add_user('zoes', '2065610s@student.gla.ac.uk', 'root'  )
+    zo.save()
     # User profiles
-    Omar = add_user_profile(om, 'Omar Tufail', 'Blah@blah.co.uk',
-                            'about me', 'Going out, playing foodball, reading.')
+    Omar = add_user_profile(om, 'Omar Tufail', 'omar@gmail.com',
+                            'about me', 'Going out, playing foodball, reading.', 'profile_images/omar.jpg')
 
     Omar.university = glasgow
-    Omar.availability.add(sat13, fri14)
+    Omar.availability.add(sat13)
+    Omar.availability.add(fri14)
+    Omar.availability.add(wed15)
     Omar.save()
 
-    Justas = add_user_profile(ju, 'Justas Bikulcius', 'Blah2@blah.co.uk',
-                                                           'about me', 'Attending lectures, not doing drugs.')
+    Justas = add_user_profile(ju, 'Justas Bikulcius', 'eustace@yahoo.com',
+                              'about me', 'Attending lectures, not doing drugs.', 'profile_images/justas.jpg')
+
     Justas.university = glasgow
     Justas.availability.add(sat13)
     Justas.availability.add(wed15)
+    Justas.availability.add(tue13)
     Justas.save()
 
-    Amy = add_user_profile(am, 'Amy Rose', 'Blah3@blah.co.uk', 'about me', 'Listening to music and dancing.')
+    Amy = add_user_profile(am, 'Amy Rose', 'xamyrosexx@aol.co.uk', 'about me', 'Listening to music and dancing.',
+                           'profile_images/amy.jpg')
 
     Amy.availability.add(wed15)
+    Amy.availability.add(fri14)
     Amy.availability.add(thu16)
     Amy.university = glasgow
     Amy.save()
 
-    #make_matches()
+    Raj = add_user_profile(ra, 'Rajeevan Vijayakumar', 'vj96@live.co.uk', 'about me', 'Playing games and reking noobs.',
+                           'profile_images/raj.jpg' )
+
+    Raj.university = glasgow
+    Raj.availability.add(sat13)
+    Raj.availability.add(thu16)
+    Raj.availability.add(tue13)
+    Raj.save()
+
+    Blair = add_user_profile(bl, 'Blair Aitcheson', 'baitch96@hotmail.co.uk', 'about me', 'Listening to music and chilling',
+                             'profile_images/blair.jpg' )
+
+    Blair.university = glasgow
+    Blair.availability.add(fri14)
+    Blair.availability.add(wed15)
+    Blair.availability.add(thu15)
+    Blair.save()
+
+    Zoe = add_user_profile(zo, 'Zoe Thorn', 'zzthorn@live.co.uk', 'about me', 'Watching Movies and going out',
+                             'profile_images/zoe.jpg' )
+
+    Zoe.university = glasgow
+    Zoe.availability.add(fri14)
+    Zoe.availability.add(wed15)
+    Zoe.availability.add(tue13)
+    Zoe.save()
+
+
     # Feedback
     add_feedback('What a charmer!', Omar, Amy)
     add_feedback('I think I still owe you 40p...', Omar, Justas)
     add_feedback('Could have been better. Asshole was late.', Justas, Omar)
     add_feedback('We both like the same band, untitled artist.', Amy, Justas)
     add_feedback('We both like the same song, unknown track.', Justas, Amy)
+    add_feedback('Had a great lunch, thanks. Hope we can grab luch again.', Zoe, Raj)
+    add_feedback('Finally found some who loves games as much as me', Raj, Zoe)
+    add_feedback('Will be seeing you again for dinner I hope', Blair, Amy)
 # Start execution here!
 if __name__ == '__main__':
     print "Starting LunchUp population script..."
