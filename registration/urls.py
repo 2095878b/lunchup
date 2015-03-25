b4ed55c7b98105f13e61883701598be37efb63ec
Justas <Justas.B94@gmail.com>
2015-03-25 15:28:56 +0000

    Minor fixes

--- registration/urls.py
+++ registration/urls.py
@@ -3,7 +3,7 @@ Backwards-compatible URLconf for existing django-registration
 installs; this allows the standard ``include('registration.urls')`` to
 continue working, but that usage is deprecated and will be removed for
 django-registration 1.0. For new installs, use
-``include('registration.backends.default.urls')``.
+``include('registration.bguitackends.default.urls')``.
 
 """
 
