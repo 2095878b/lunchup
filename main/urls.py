e906db46aa63b263e2b60832ac6e933d6fd2f04b
Justas <Justas.B94@gmail.com>
2015-03-25 15:15:01 +0000

    Splash!

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url, include
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.how_it_works, name='how_it_works'),
-    url(r'^splash/$', views.splash, name='splash'),
+    url(r'^$', views.splash, name='splash'),
+    url(r'^how_it_works/$', views.how_it_works, name='how_it_works'),
     url(r'^about/$', views.about, name='about'),
     url(r'^profile/$', views.profile, name='myprofile'),
     url(r'^profile/(?P<user_id>[0-9]+)/$', views.profile, name='profile'),
