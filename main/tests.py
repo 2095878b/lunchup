a79b12f8a257d6a23d548208ffebf99936fbf53d
2078910A <baitch1996@gmail.com>
2015-03-27 21:28:21 +0000

    Notification test added

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from main.models import University, UserProfile, Feedback, Notification, TimeInterval
+from main.views import magic
 from django.core.urlresolvers import reverse
 
 class UniversityMethodTests(TestCase):
@@ -13,7 +14,7 @@ class UniversityMethodTests(TestCase):
 
 class UserProfileTests(TestCase):
 
-    def check_person_is_from_glasgow(self):
+    def test_check_person_is_from_glasgow(self):
 
         prof = UserProfile(user='sam', fullName = 'Samantha Jones', publicEmail="lalasam@gmail.com",
                             interests = '', about="")
@@ -22,8 +23,32 @@ class UserProfileTests(TestCase):
 
 class TimeIntervalTests(TestCase):
 
-    def check_day_is_a_day(self):
+    def test_check_day_is_a_day(self):
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
         new_day = TimeInterval(time=14, day="Monday")
         self.assertIn(new_day.day,days,True)
 
+class NotificationTests(TestCase):
+
+    def test_page_displays_no_matches(self):
+
+        response = self.client.get(reverse('notifications'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response, 'No notifications yet.')
+
+    def setup(self):
+        dave = UserProfile.objects.get_or_create(user = 'dave', fullName = 'Dave Smith', publicEmail="something@student.gla.ac.uk", interests='', about='')
+        steve = UserProfile.objects.get_or_create(user = 'steve', fullName = 'Steve Smith', publicEmail="somethingelse@student.gla.ac.uk", interests='', about='')
+
+        timeSlot = TimeInterval.obects.get_or_create(time=13, day="Tuesday")
+
+        dave.availability = timeSlot
+        steve.availability = timeSlot
+
+        magic()
+
+    def test_notifications_are_working(self):
+        
+        response = self.client.get(reverse('notifications'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(response, 'No notifications yet')
